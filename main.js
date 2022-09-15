6dccfe804fba4058557800aceecb9d2ace622db3
Nathanael Roviery <roviery@gmail.com>
2022-09-15 18:32:26 +0700

    refactor: main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+var gl, canvas, shaderProgram;
+
 function checkWebGL(canvas) {
     var contexts = ["webgl", "expreimental-webgl", "webkit-3d", "moz-webggl"], gl;
-    for (var i = 0; i<contexts.length; i++){
-        try{
+    for (var i = 0; i < contexts.length; i++) {
+        try {
             gl = canvas.getContext(contexts[i]);
-        } catch(e) {}
+        } catch (e) { }
         if (gl) {
             break;
         }
     }
-    if (!gl){
+    if (!gl) {
         alert("WebGL not available, sorry! Please use a new version of Chrome or Firefox.");
     }
     return gl;
@@ -16,23 +18,7 @@ function checkWebGL(canvas) {
 
 function main() {
     var canvas = document.getElementById("my-canvas");
-    var gl = checkWebGL(canvas);
-
-    var rVertices = [
-        -0.9, 0.75,      // 1
-        -0.9, -0.75,     // 2
-        -0.7, -0.75,     // 3
-        -0.7, -0.25,     // 4
-        -0.65, -0.75,       // 5
-        -0.45, -0.75,     // 6
-        -0.55, -0.1,     // 7
-        -0.4, 0.325,
-        -0.55, 0.75
-    ];
-
-    var buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(rVertices), gl.STATIC_DRAW);
+    gl = checkWebGL(canvas);
 
     // Vertex Shader
     var vertexShaderCode = `
@@ -65,20 +51,58 @@ function main() {
 
 
     // Shader Program
-    var shaderProgram = gl.createProgram(); // wadah dari executable (.exe)
+    shaderProgram = gl.createProgram(); // wadah dari executable (.exe)
     gl.attachShader(shaderProgram, vertexShaderObject);
     gl.attachShader(shaderProgram, fragmentShaderObject);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(aPosition)
-
     gl.clearColor(0.4, 0.7, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 9);
-    gl.drawArrays(gl.LINE_LOOP, 0, 9);
+    drawR();
+}
 
+function drawR() {
+    var n = initRBuffers();
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+    }
+    gl.drawArrays(gl.POINTS, 0, n);
+    gl.drawArrays(gl.LINE_LOOP, 0, n);
+}
+
+function initRBuffers() {
+    var vertices = new Float32Array([
+        -0.9, 0.75,     // 1
+        -0.9, -0.75,    // 2
+        -0.7, -0.75,    // 3
+        -0.7, -0.25,    // 4
+        -0.65, -0.75,   // 5
+        -0.45, -0.75,   // 6
+        -0.55, -0.1,    // 7
+        -0.4, 0.325,    // 8
+        -0.55, 0.75     // 9
+    ]);
+    var n = 9;
+
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var aPosition = gl.getAttribLocation(shaderProgram, 'aPosition');
+    if (aPosition < 0) {
+        console.log('Failed to get the storage location of aPosition');
+        return -1;
+    }
+
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+    return n;
 }
